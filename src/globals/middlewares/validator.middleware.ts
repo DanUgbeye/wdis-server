2b3e7b3cbd4aca6ad2235a14672b0f77650aaef9
Daniel Ugbeye <ugbeyellionz@gmail.com>
2023-09-08 01:34:22 +0100

    feat: complete validation middleware and rename notFoundMiddleware function

--- src/globals/middlewares/validator.middleware.ts
+++ src/globals/middlewares/validator.middleware.ts
@@ -1,28 +1,39 @@
 import { NextFunction, Request, Response } from "express";
 import { ValidationException } from "../exceptions";
 import { RequestValidationSchemas } from "../../types/validation.types";
+import { ZodError } from "zod";
+import errorUtility from "../utils/error";
 
 export default function validateRequest(schemas: RequestValidationSchemas) {
   return async function (req: Request, res: Response, next: NextFunction) {
     try {
       if (schemas.body) {
-        let body = await schemas.body.parseAsync(req.body);
+        let body = schemas.body.parse(req.body);
         req.body = body;
       }
 
       if (schemas.params) {
-        let params = await schemas.params.parseAsync(req.params);
+        let params = schemas.params.parse(req.params);
         req.params = params;
       }
 
       if (schemas.query) {
-        let query = await schemas.query.parseAsync(req.query);
+        let query = schemas.query.parse(req.query);
         req.query = query;
       }
-    } catch (error: any) {
-      console.log(error);
+
+      next();
+    } catch (error: any | ZodError[]) {
       let errMessage: string = "validation failed";
-      next(new ValidationException(errMessage));
+      let errors;
+
+      if (error instanceof ZodError) {
+        errors = errorUtility.formatZodError(error);
+        errMessage = errorUtility.getFirstErrorMessage(errors);
+      } else {
+        errMessage = error.message;
+      }
+      next(new ValidationException(errMessage, errors));
     }
   };
 }
