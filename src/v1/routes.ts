15ec0056151f2b83b1ac03a785c3040191656046
Daniel Ugbeye <ugbeyellionz@gmail.com>
2023-09-09 11:42:33 +0100

    task: integrate new user router

--- src/v1/routes.ts
+++ src/v1/routes.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import authRouter from "./modules/auth/auth.routes";
+import userRouter from "./modules/user/user.routes";
 
 const apiV1Routes = express.Router();
 
@@ -7,6 +8,7 @@ apiV1Routes.get("/", (req, res) => {
   res.send(`⚡ API V1 up and running ⚡`);
 });
 
-apiV1Routes.use("/auth", authRouter)
+apiV1Routes.use("/auth", authRouter);
+apiV1Routes.use("/user", userRouter);
 
 export default apiV1Routes;
