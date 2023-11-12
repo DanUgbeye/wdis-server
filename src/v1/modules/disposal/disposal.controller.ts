5c03fc6b2eee6ba3c24fcc83ba3f723a4c515bf7
Daniel Ugbeye <ugbeyellionz@gmail.com>
2023-11-12 21:25:17 +0100

    task: create disposal module

---
+++ src/v1/modules/disposal/disposal.controller.ts
@@ -0,0 +1,87 @@
+import { Request, Response } from "express";
+import disposalModel from "./disposal.model";
+import ApiResponse from "../../../globals/helpers/apiResponse";
+import { DisposalData } from "./disposal.types";
+import { BaseException } from "src/globals/exceptions";
+
+export class DisposalController {
+  /**
+   * get disposal data
+   * @route GET .../disposal/:id
+   */
+  async findById(req: Request<any, any, any, any>, res: Response) {
+    const id = req.params.id as string;
+    let disposal;
+
+    try {
+      disposal = await disposalModel.findById(id);
+    } catch (error: any | Error | BaseException) {
+      return ApiResponse.create(res).error(error);
+    }
+
+    return ApiResponse.create(res).success(
+      "disposal found successfully",
+      disposal
+    );
+  }
+
+  /**
+   * get disposal stats
+   * @route GET .../disposal/stats
+   */
+  async getStats(req: Request<any, any, any, any>, res: Response) {
+    let stats;
+
+    try {
+      stats = await disposalModel.getDisposalStats();
+    } catch (error: any | BaseException) {
+      return ApiResponse.create(res).error(error);
+    }
+
+    return ApiResponse.create(res).success("disposal stats retrieved", stats);
+  }
+
+  /**
+   * update disposal
+   * @route PATCH .../disposal/:id
+   */
+  async update(req: Request<any, any, any, any>, res: Response) {
+    const id = req.params.id as string;
+    const updateData = req.body.status as Partial<DisposalData>;
+    let updatedDisposal;
+
+    try {
+      updatedDisposal = await disposalModel.update(id, updateData);
+    } catch (error: any | Error | BaseException) {
+      return ApiResponse.create(res).error(error);
+    }
+
+    return ApiResponse.create(res).success(
+      "disposal updated successfully",
+      updatedDisposal
+    );
+  }
+
+  /**
+   * delete disposal
+   * @route DELETE .../disposal/:id
+   */
+  async delete(req: Request<any, any, any, any>, res: Response) {
+    const id = req.params.id as string;
+
+    try {
+      await disposalModel.delete(id);
+    } catch (error: any | Error | BaseException) {
+      return ApiResponse.create(res).error(error);
+    }
+
+    return ApiResponse.create(res).success(
+      "disposal deleted successfully",
+      200
+    );
+  }
+}
+
+const disposalController = new DisposalController();
+
+export default disposalController;
---
+++ src/v1/modules/disposal/disposal.types.ts
@@ -0,0 +1,17 @@
+import { Document } from "mongoose";
+
+export type DisposalStatus =
+  (typeof DISPOSAL_STATUS)[keyof typeof DISPOSAL_STATUS];
+
+export const DISPOSAL_STATUS = {
+  ONGOING: "ongoing",
+  COMPLETED: "completed",
+} as const;
+
+export interface DisposalData {
+  binId: string;
+  status: DisposalStatus;
+  disposedAt: Date;
+}
+
+export interface DisposalDocument extends Document, DisposalData {}
