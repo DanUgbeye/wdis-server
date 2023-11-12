573c145ddbb9b0d1fad802876b93a76f3dda91c9
Daniel Ugbeye <ugbeyellionz@gmail.com>
2023-11-12 21:02:16 +0100

    task: rename server config file, add new fields

--- src/globals/config/server.config.ts
+++ src/globals/config/server.config.ts
@@ -8,11 +8,18 @@ class ServerConfig {
   /** refresh token secret for signing JWt tokens */
   public readonly REFRESH_TOKEN_SECRET: string;
 
+  /** server url */
+  public readonly SERVER_BASE_URL: string;
+  /** server url */
+  public readonly CLIENT_BASE_URL: string;
+
   constructor() {
     this.PORT = process.env.PORT ? Number(process.env.PORT) : 5000;
     this.MONGO_URI = process.env.MONGO_URI as string;
     this.ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET as string;
     this.REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET as string;
+    this.SERVER_BASE_URL = process.env.SERVER_BASE_URL as string;
+    this.CLIENT_BASE_URL = process.env.CLIENT_BASE_URL as string;
   }
 }
 
